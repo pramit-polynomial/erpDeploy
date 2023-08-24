fc51d15717afc103c627d65df317b728c067a9eb
pramit-polynomial <pramit@polynomial.ai>
2023-08-24 18:00:03 +0530

    add render url

--- index.js
+++ index.js
@@ -1,62 +1,11 @@
-// const express = require('express');
-// const passport = require('passport');
-// const GoogleStrategy = require('passport-google-oauth20').Strategy;
-// const session = require('express-session'); // Import express-session
-
-// const app = express();
-
-// // Replace with your own Google OAuth credentials
-// const GOOGLE_CLIENT_ID = '401126326913-mfv3iq0hoked99jljckhsh6gcoe4765t.apps.googleusercontent.com';
-// const GOOGLE_CLIENT_SECRET = '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';
-
-// // Set up Google OAuth strategy
-// passport.use(new GoogleStrategy({
-//   clientID: GOOGLE_CLIENT_ID,
-//   clientSecret: GOOGLE_CLIENT_SECRET,
-//   callbackURL: 'http://localhost:8000/auth/google/callback',
-//   // userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
-// }, (accessToken, refreshToken, profile, done) => {
-//   // Handle user profile data
-//   // 'profile' contains user information
-// }));
-// app.use(session({
-//     secret: GOOGLE_CLIENT_SECRET, // Change this to a secure secret
-//     resave: false,
-//     saveUninitialized: true
-//   }));
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// // Authentication route
-// app.get('/auth/google',
-//   passport.authenticate('google', { scope: ['profile', 'email'] })
-// );
-
-// // Callback route
-// app.get('/auth/google/callback',
-//   passport.authenticate('google', { failureRedirect: '/' }),
-//   (req, res) => {
-//     // Successful authentication
-//     console.log("callback url")
-//     res.redirect('http://localhost:8000/profile');
-//   }
-// );
-
-// app.get('/profile', (req, res) => {
-//   // Display user profile page
-//   res.send('Welcome to your profile');
-// });
-
-// app.listen(8000, () => {
-//   console.log('Server started on http://localhost:8000');
-// });
-
 
 const express = require('express');
 const { google } = require('googleapis');
 const session = require('express-session');
+const fs = require('fs');
 
 const app = express();
+const RANGE_NAME = 'Sheet1!A1:B10';
 
 // Replace with your own Google OAuth credentials
 const GOOGLE_CLIENT_ID = '215321644651-34frhpudrs7jl76rbd7ehisklkfejb6c.apps.googleusercontent.com';
@@ -86,9 +35,26 @@ app.get('/auth/google', (req, res) => {
 });
 
 app.get('/auth/google/callback', async (req, res) => {
-  const { code } = req.query;
-  const { tokens } = await oauth2Client.getToken(code);
-  oauth2Client.setCredentials(tokens);
+  const code = req.query.code;
+
+  try {
+    const { tokens } = oauth2Client.getToken(code);
+    oauth2Client.setCredentials(tokens);
+
+    const people = google.people({ version: 'v1', auth: oauth2Client });
+    const { data } = await people.people.get({
+      resourceName: 'people/me',
+      personFields: 'emailAddresses'
+    });
+    console.log("data", data)
+    const userEmail = data.emailAddresses[0].value;
+    console.log("userEmail", userEmail)
+    // Now you can send the userEmail to the frontend
+    res.send({ email: userEmail });
+  } catch (error) {
+    console.error('Error retrieving user email:', error);
+    res.status(500).send('Error retrieving user email');
+  }
 
   // You can use the tokens to access the user's Google services
 
@@ -99,6 +65,26 @@ app.get('/profile', (req, res) => {
   // Display user profile page
   res.send('Welcome to your profile');
 });
+
+
+
+
+// // get all sheet 
+// app.get('/sheets', async (req, res) => {
+//   try {
+
+//     const credentials = {
+//       client_email: "pramit@polynomial.ai"
+//     };
+//     const drive = new GoogleDrive(auth.getCredentials(credentials));
+
+
+//   } catch (err) {
+//     console.log(err)
+//   }
+// })
+
+
 
 app.listen(8000, () => {
   console.log('Server started on http://localhost:8000');
